49e8a272998b6410658f532a819320fa03b5824a
g0401828t <g0401828t@gmail.com>
2021-09-29 23:46:23 +0900

    second commit

--- hog.py
+++ hog.py
@@ -1,46 +1,42 @@
-# #importing required libraries
-# from skimage.io import imread
-# from skimage.transform import resize
-# from skimage.feature import hog
-# import matplotlib.pyplot as plt
+## for simple visualization
+"""
+#importing required libraries
+from skimage.io import imread
+from skimage.transform import resize
+from skimage.feature import hog
+import matplotlib.pyplot as plt
 
-# #reading the image
-# img = imread('pedestrians.jpg')
+#reading the image
+img = imread('pedestrians.jpg')
+plt.axis("off")
+plt.imshow(img)
+print(img.shape)
+
+# resizing image
+resized_img = resize(img, (128*4, 64*4))
 # plt.axis("off")
-# plt.imshow(img)
-# print(img.shape)
+# plt.imshow(resized_img)
+# plt.show()
+print(resized_img.shape)
 
-# # resizing image
-# resized_img = resize(img, (128*4, 64*4))
-# # plt.axis("off")
-# # plt.imshow(resized_img)
-# # plt.show()
-# print(resized_img.shape)
+## creating hog features
+fd, hog_image = hog(resized_img, orientations=9, pixels_per_cell=(8, 8),
+                	cells_per_block=(2, 2), visualize=True, multichannel=True)
+print(fd.shape)
+print(hog_image.shape)
+# plt.axis("off")
+# plt.imshow(hog_image, cmap="gray")
+# plt.show()
 
-# ## creating hog features
-# fd, hog_image = hog(resized_img, orientations=9, pixels_per_cell=(8, 8),
-#                 	cells_per_block=(2, 2), visualize=True, multichannel=True)
-# print(fd.shape)
-# print(hog_image.shape)
-# # plt.axis("off")
-# # plt.imshow(hog_image, cmap="gray")
-# # plt.show()
-
-
-# ## Creating derivatives
+# save the images
+plt.imsave("resized_img.jpg", resized_img)
+plt.imsave("hog_image.jpg", hog_image, cmap="gray")
+"""
 
 
 
 
-# # save the images
-# plt.imsave("resized_img.jpg", resized_img)
-# plt.imsave("hog_image.jpg", hog_image, cmap="gray")
-
-
-
-
-
-
+## implemented in scratch
 import os, sys
 import matplotlib.pyplot as plt
 import matplotlib.image as iread
